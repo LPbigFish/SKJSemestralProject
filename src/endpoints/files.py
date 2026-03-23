4b5f4056ae924552c66f7f85cebab636988b0706
LPbigFish <lpbyblock@gmail.com>
2026-03-23 11:29:31 +0100

    status_codes

--- src/endpoints/files.py
+++ src/endpoints/files.py
@@ -2,18 +2,18 @@ from fastapi import APIRouter
 
 files_router = APIRouter(prefix="/files")
 
-@files_router.get("/")
+@files_router.get("/", status_code=200)
 def get_files():
     ...
 
-@files_router.get("/{file_id}")
-def get_specific_file():
+@files_router.get("/{user_id}/{file_id}", status_code=200)
+def get_specific_file(user_id, file_id):
     ...
     
-@files_router.delete("/{file_id}")
-def delete_specific_file():
+@files_router.delete("/{user_id}/{file_id}", status_code=204)
+def delete_specific_file(user_id, file_id):
     ...
 
-@files_router.post("/upload")
+@files_router.post("/upload", status_code=201)
 def create_file():
     ...
