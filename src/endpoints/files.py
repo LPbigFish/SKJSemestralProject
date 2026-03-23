b35f4454662144a90e54ee09a30c25eb82342813
LPbigFish <lpbyblock@gmail.com>
2026-03-23 11:24:27 +0100

    route structure

--- src/endpoints/files.py
+++ src/endpoints/files.py
@@ -1,7 +1,19 @@
 from fastapi import APIRouter
 
-files_router = APIRouter()
+files_router = APIRouter(prefix="/files")
 
-@files_router.get("/files")
+@files_router.get("/")
 def get_files():
+    ...
+
+@files_router.get("/{file_id}")
+def get_specific_file():
+    ...
+    
+@files_router.delete("/{file_id}")
+def delete_specific_file():
+    ...
+
+@files_router.post("/upload")
+def create_file():
     ...
